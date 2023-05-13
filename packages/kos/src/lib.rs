939aab70dce41345e97b127e56ada1ddf40f0ca9
fbsobreira <fbsobreira@klever.io>
2023-05-13 13:44:27 +0000

    JsError to Error for generate mnemonic

--- packages/kos/src/lib.rs
+++ packages/kos/src/lib.rs
@@ -1,9 +1,10 @@
 use kos_crypto::mnemonic::generate_mnemonic;
-pub use kos_sdk::wallet::Wallet;
+pub use kos_sdk::*;
+use kos_types::error::Error;
 use wasm_bindgen::prelude::*;
 
 /// Generates a random mnemonic phrase given the number of words to generate, `count`.
 #[wasm_bindgen(js_name = "generateMnemonicPhrase")]
-pub fn generate_mnemonic_phrase(count: usize) -> Result<String, JsError> {
+pub fn generate_mnemonic_phrase(count: usize) -> Result<String, Error> {
     Ok(generate_mnemonic(count)?.to_phrase())
 }
