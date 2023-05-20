3a66af9b2fa29308a0831c322b13b9ac51998fde
fbsobreira <fbsobreira@klever.io>
2023-05-20 13:03:02 +0000

    Add logger

--- packages/kos/src/lib.rs
+++ packages/kos/src/lib.rs
@@ -1,8 +1,21 @@
 use kos_crypto::mnemonic::generate_mnemonic;
 pub use kos_sdk::*;
 use kos_types::error::Error;
+use kos_utils::logger;
 
 use wasm_bindgen::prelude::*;
+
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+
+    logger::init(logger::Config::default());
+
+    log::info!("KOS Module initialized");
+
+    Ok(())
+}
 
 /// Generates a random mnemonic phrase given the number of words to generate, `count`.
 #[wasm_bindgen(js_name = "generateMnemonicPhrase")]
