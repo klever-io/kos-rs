6bd000b0d4a91be9f5b42235e2fa0f722309dd7a
fbsobreira <fbsobreira@klever.io>
2023-05-31 15:09:14 +0000

    http user agent && timeout

--- packages/kos-sdk/src/utils.rs
+++ packages/kos-sdk/src/utils.rs
@@ -10,15 +10,28 @@ pub fn http_get_block<T: DeserializeOwned>(url: String) -> Result<T, Error> {
     body.json::<T>().map_err(|e| Error::from(e))
 }
 
+pub fn get_client() -> Result<reqwest::Client, Error> {
+    reqwest::Client::builder()
+        .user_agent("kos-rs/0.0.1")
+        .connect_timeout(std::time::Duration::from_secs(20))
+        .timeout(std::time::Duration::from_secs(100))
+        .build()
+        .map_err(|e| Error::ReqwestError(e.to_string()))
+}
+
 pub async fn http_get<T: DeserializeOwned>(url: String) -> Result<T, Error> {
-    let body = reqwest::get(url).await?;
+    let client = get_client()?;
+
+    let body = client.get(url).send().await?;
     body.json::<T>().await.map_err(|e| Error::from(e))
 }
 
 pub async fn http_post<T: DeserializeOwned>(url: String, data: &Vec<u8>) -> Result<T, Error> {
-    let client = reqwest::Client::new();
+    let client = get_client()?;
+
     client
         .post(url)
+        .header("Content-Type", "application/json")
         .body(data.to_vec())
         .send()
         .await
