44d1459346459d93bdf207565ce35d7b90cf51a5
fbsobreira <fbsobreira@klever.io>
2023-06-03 11:46:47 +0000

    break encrypt/decrypt & pem encoding

--- packages/kos-crypto/src/cipher.rs
+++ packages/kos-crypto/src/cipher.rs
@@ -17,6 +17,15 @@ const NONCE_SIZE: usize = 12; // NONCESIZE
 
 const ITERATIONS: u32 = 10000;
 
+pub fn to_pem(tag: String, data: &[u8]) -> Result<Pem, Error> {
+    Ok(Pem::new(tag, data))
+}
+
+pub fn from_pem(pem: Pem) -> Vec<u8> {
+    pem.contents().to_vec()
+}
+
+
 pub fn derive_key(salt: &[u8], password: &str) -> Vec<u8> {
     let mut key = vec![0u8; KEY_SIZE];
     pbkdf2::<Hmac<Sha256>>(password.as_bytes(), salt, ITERATIONS, &mut key)
@@ -24,7 +33,7 @@ pub fn derive_key(salt: &[u8], password: &str) -> Vec<u8> {
     key
 }
 
-pub fn encrypt(data: &[u8], password: &str, tag: &str) -> Result<Pem, Error> {
+pub fn encrypt(data: &[u8], password: &str) -> Result<Vec<u8>, Error> {
     // Derive key from password
     let salt: [u8; 32] = rand::thread_rng().gen();
     let derived_key = derive_key(&salt, password);
@@ -44,17 +53,16 @@ pub fn encrypt(data: &[u8], password: &str, tag: &str) -> Result<Pem, Error> {
     result.extend_from_slice(&nonce);
     result.extend_from_slice(&ciphertext);
 
-    Ok(Pem::new(tag, result))
+    Ok(result)
 }
 
-pub fn decrypt(pem: Pem, password: &str) -> Result<Vec<u8>, Error> {
-    let all = pem.contents();
-    if all.len() < KEY_SIZE + NONCE_SIZE {
+pub fn decrypt(encrypted: &[u8], password: &str) -> Result<Vec<u8>, Error> {
+    if encrypted.len() < KEY_SIZE + NONCE_SIZE {
         return Err(Error::CipherError("Invalid PEM data".to_owned()));
     }
-    let salt = &all[..KEY_SIZE];
-    let nonce = &all[KEY_SIZE..KEY_SIZE + NONCE_SIZE];
-    let encrypted_data = &all[KEY_SIZE + NONCE_SIZE..];
+    let salt = &encrypted[..KEY_SIZE];
+    let nonce = &encrypted[KEY_SIZE..KEY_SIZE + NONCE_SIZE];
+    let encrypted_data = &encrypted[KEY_SIZE + NONCE_SIZE..];
     let derived_key = derive_key(&salt, password);
     let key = Key::<Aes256Gcm>::from_slice(&derived_key);
     let cipher = Aes256Gcm::new(key);
@@ -71,10 +79,9 @@ mod tests {
     fn test_encrypt_decrypt() {
         let data = b"hello world";
         let password = "password";
-        let tag = "tag";
 
-        let pem = encrypt(data, password, tag).unwrap();
-        let decrypted = decrypt(pem, password).unwrap();
+        let encrypted = encrypt(data, password).unwrap();
+        let decrypted = decrypt(&encrypted, password).unwrap();
         assert_eq!(data, decrypted.as_slice());
     }
 
@@ -82,10 +89,37 @@ mod tests {
     fn test_encrypt_decrypt_invalid_password() {
         let data = b"hello world";
         let password = "password";
-        let tag = "tag";
 
-        let pem = encrypt(data, password, tag).unwrap();
-        let decrypted = decrypt(pem, "invalid password");
+        let encrypted = encrypt(data, password).unwrap();
+        let decrypted = decrypt(&encrypted, "invalid password");
         assert!(decrypted.is_err());
+    }
+
+    #[test]
+    fn test_encrypt_decrypt_invalid_data() {
+        let data = b"hello world";
+        let password = "password";
+
+        let encrypted = encrypt(data, password).unwrap();
+        let decrypted = decrypt(&encrypted[..encrypted.len() - 1], password);
+        assert!(decrypted.is_err());
+    }
+
+    #[test]
+    fn test_to_pem() {
+        let data = b"hello world";
+        let tag = "TEST";
+        let pem = to_pem(tag.to_owned(), data).unwrap();
+        assert_eq!(pem.tag(), tag);
+        assert_eq!(pem.contents(), data);
+    }
+
+    #[test]
+    fn test_from_pem() {
+        let data = b"hello world";
+        let tag = "TEST";
+        let pem = to_pem(tag.to_owned(), data).unwrap();
+        let pem_data = from_pem(pem);
+        assert_eq!(pem_data, data);
     }
 }
