8d921cd50adca3a60f07fdfee368ce7a65fba41f
fbsobreira <fbsobreira@klever.io>
2023-06-05 21:45:23 +0000

    Default Mnemonic/Refactor LockUnlock/Add Tests

--- packages/kos-sdk/src/wm.rs
+++ packages/kos-sdk/src/wm.rs
@@ -11,12 +11,12 @@ use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 
 const KOS_WM_TAG: &str = "KOS WALLET MANAGER";
+const KOS_WM_TAG_DEFAULT: &str = "KOS WM DEFAULT";
 
 #[wasm_bindgen]
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct WalletManager {
     is_locked: bool,
-    default_mn: Option<String>,
     encrypted_data: Option<Vec<u8>>,
     checksum: Option<Vec<u8>>,
     // skip serializing wallets, all data must be encrypted into encrypted_data
@@ -30,7 +30,6 @@ impl WalletManager {
     pub fn new() -> WalletManager {
         WalletManager {
             is_locked: false,
-            default_mn: None,
             encrypted_data: None,
             checksum: None,
             wallets: HashMap::new(),
@@ -112,33 +111,38 @@ impl WalletManager {
             return Ok(());
         }
 
-        // verify password if encrypted data is present, else encrypt and save data
-        match self.encrypted_data {
-            Some(_) => self.verify_password(password.clone())?,
-            None => {
-                let mut wallets: Vec<Pem> = Vec::new();
-                // serialize all wallets and save to encrypted_data
-                for (_, wallet) in self.wallets.iter_mut() {
-                    if !wallet.is_locked() {
-                        wallet.lock(password.clone())?;
-                    }
-
-                    let pem = wallet.export(password.clone())?;
-                    wallets.push(pem);
-                }
-
-                let data = encode_many_pem(&wallets);
-                self.encrypted_data = Some(data.as_bytes().to_vec());
-                self.checksum = Some(
-                    kos_crypto::cipher::create_checksum(&password)
-                        .as_bytes()
-                        .to_vec(),
-                );
-            }
+        // verify password if encrypted data is present
+        if let Some(_) = &self.encrypted_data {
+            self.verify_password(password.clone())?;
         }
 
+        let wallets: Result<Vec<Pem>, Error> = self
+            .wallets
+            .iter_mut()
+            .map(|(tag, wallet)| {
+                wallet.lock(password.clone())?;
+                let w = wallet.export(password.clone())?;
+
+                // replace default tag with KOS_WM_TAG_DEFAULT
+                Ok(if tag == KOS_WM_TAG_DEFAULT {
+                    Pem::new(KOS_WM_TAG_DEFAULT, w.contents())
+                } else {
+                    w
+                })
+            })
+            .collect();
+
+        let wallets = wallets?;
+
+        self.encrypted_data = Some(encode_many_pem(&wallets).as_bytes().to_vec());
+        self.checksum = Some(
+            kos_crypto::cipher::create_checksum(&password)
+                .as_bytes()
+                .to_vec(),
+        );
+
         // reset secrets
-        self.wallets = HashMap::new();
+        self.wallets.clear();
         self.is_locked = true;
 
         Ok(())
@@ -147,7 +151,7 @@ impl WalletManager {
 
 impl WalletManager {
     pub fn import(pem: Pem) -> Result<WalletManager, Error> {
-        //check tag
+        // check tag
         if pem.tag() != KOS_WM_TAG {
             return Err(Error::WalletManagerError("Invalid PEM tag".to_string()));
         }
@@ -181,12 +185,71 @@ impl WalletManager {
 
 #[wasm_bindgen]
 impl WalletManager {
-    // todo!() save operation
+    fn ensure_unlocked(&mut self, password: String) -> Result<(), Error> {
+        if !self.is_locked() {
+            self.unlock(password)
+        } else {
+            self.verify_password(password)
+        }
+    }
+
     #[wasm_bindgen(js_name = "addWallet")]
-    pub fn add_wallet(&mut self, wallet: Wallet) -> Result<(), Error> {
-        let wallet_name = Wallet::wallet_key(wallet.get_chain(), wallet.get_address().as_str());
-        self.wallets.insert(wallet_name, wallet);
-        Ok(())
+    pub fn add_wallet(&mut self, wallet: Wallet, password: String) -> Result<Wallet, Error> {
+        self.ensure_unlocked(password.clone())?;
+
+        let mut wallet = wallet.clone();
+
+        // check if wallet is locked
+        if !wallet.is_locked() {
+            wallet.lock(password.clone())?;
+        } else {
+            wallet.verify_password(password.clone())?;
+        }
+
+        let wallet_name = wallet.get_key();
+
+        // error if exists
+        if self.wallets.contains_key(&wallet_name) {
+            return Err(Error::WalletManagerError(
+                "Wallet already exists".to_string(),
+            ));
+        }
+
+        self.wallets.insert(wallet_name, wallet.clone());
+
+        Ok(wallet)
+    }
+
+    #[wasm_bindgen(js_name = "newWallet")]
+    pub fn new_wallet(
+        &mut self,
+        chain: crate::chain::Chain,
+        password: String,
+    ) -> Result<Wallet, Error> {
+        self.ensure_unlocked(password.clone())?;
+
+        let index = self
+            .wallets
+            .values()
+            .filter(|wallet| {
+                wallet.get_chain() == chain
+                    && wallet.get_account_type() == crate::wallet::AccountType::Mnemonic
+            })
+            .filter_map(|wallet| wallet.get_index().ok())
+            .max()
+            .map(|x| x + 1)
+            .unwrap_or(0);
+
+        let mut wallet = Wallet::from_mnemonic_index(
+            chain,
+            self.get_mnemonic(password.to_owned())?,
+            index,
+            None,
+        )?;
+
+        wallet.lock(password.clone())?;
+
+        self.add_wallet(wallet, password)
     }
 
     #[wasm_bindgen(js_name = "removeWallet")]
@@ -216,21 +279,87 @@ impl WalletManager {
             ))),
         }
     }
+
+    #[wasm_bindgen(js_name = "getMnemonic")]
+    pub fn get_mnemonic(&self, password: String) -> Result<String, Error> {
+        // error if unlocked
+        if self.is_locked() {
+            return Err(Error::WalletManagerError(
+                "WalletManager is locked".to_string(),
+            ));
+        }
+
+        // reload encrypted wallets from encrypted_data
+        let mut w_mnemonic = self
+            .wallets
+            .get(KOS_WM_TAG_DEFAULT)
+            .ok_or(Error::WalletManagerError(
+                "Default mnemonic not found".to_string(),
+            ))?
+            .to_owned();
+
+        // decrypt mnemonic
+        w_mnemonic.unlock(password.clone())?;
+        let mnemonic_str = w_mnemonic.get_mnemonic();
+        if mnemonic_str.is_empty() {
+            return Err(Error::WalletManagerError(
+                "Default mnemonic not found".to_string(),
+            ));
+        }
+
+        Ok(mnemonic_str)
+    }
+
+    #[wasm_bindgen(js_name = "setMnemonic")]
+    pub fn set_mnemonic(&mut self, mnemonic: String, password: String) -> Result<(), Error> {
+        // error if unlocked
+        if self.is_locked() {
+            return Err(Error::WalletManagerError(
+                "WalletManager is locked".to_string(),
+            ));
+        }
+
+        // error if default mnemonic exists
+        if self.wallets.contains_key(KOS_WM_TAG_DEFAULT) {
+            return Err(Error::WalletManagerError(
+                "Default mnemonic already exists".to_string(),
+            ));
+        }
+
+        // create wallet
+        let mut wallet = Wallet::from_mnemonic(
+            crate::chain::Chain::NONE,
+            mnemonic.clone(),
+            "".to_string(),
+            None,
+        )?;
+
+        wallet.lock(password.clone())?;
+        self.wallets.insert(KOS_WM_TAG_DEFAULT.to_owned(), wallet);
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::chain::Chain;
+    const DEFAULT_MNEMONIC: &str =
+        "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
 
     #[test]
     fn test_wallet_manager_export_import() {
-        let default_password = "password";
+        let default_password = "password".to_string();
 
         let mut wm = WalletManager::new();
         let wallet = Wallet::new(Chain::KLV).unwrap();
-        let result = wm.add_wallet(wallet.to_owned());
+        let result = wm.add_wallet(wallet.to_owned(), default_password.to_owned());
         assert!(result.is_ok());
+
+        // set default mnemonic
+        wm.set_mnemonic(DEFAULT_MNEMONIC.to_string(), default_password.to_string())
+            .unwrap();
 
         let result = wm.export(default_password.to_owned());
         assert_eq!(
@@ -249,24 +378,29 @@ mod tests {
         // unlock wallet manager
         let result = wm2.unlock(default_password.to_owned());
         assert!(result.is_ok());
-        assert_eq!(wm2.wallets.len(), 1);
+        // count should be 2 dues to default mnemonic wallet
+        assert_eq!(wm2.wallets.len(), 2);
 
         // check if wallet address matches
         let wallet2 = wm2
             .get_wallet(wallet.get_chain(), wallet.get_address())
             .unwrap();
         assert_eq!(wallet2.get_address(), wallet.get_address());
+
+        // check default mnemonic matchs
+        let mnemonic = wm2.get_mnemonic(default_password.to_owned()).unwrap();
+        assert_eq!(mnemonic, DEFAULT_MNEMONIC);
     }
 
     #[test]
     fn test_wallet_manager_lock_unlock() {
-        let default_password = "password";
+        let default_password = "password".to_string();
 
         let mut wm = WalletManager::new();
         let wallet = Wallet::new(Chain::KLV).unwrap();
         let test_privete_key = wallet.get_private_key();
 
-        let resul = wm.add_wallet(wallet.clone());
+        let resul = wm.add_wallet(wallet.clone(), default_password.to_owned());
         assert!(resul.is_ok());
         let result = wm.lock(default_password.to_owned());
         assert!(result.is_ok());
@@ -299,7 +433,7 @@ mod tests {
 
     #[test]
     fn test_wallet_manager_wallet_timing() {
-        let default_password = "password";
+        let default_password = "password".to_string();
         const WALLET_COUNT: usize = 10;
 
         let start_time = std::time::Instant::now();
@@ -309,7 +443,7 @@ mod tests {
         let start_time = std::time::Instant::now();
         for _ in 0..WALLET_COUNT {
             let wallet = Wallet::new(Chain::KLV).unwrap();
-            _ = wm.add_wallet(wallet.to_owned());
+            _ = wm.add_wallet(wallet.to_owned(), default_password.to_owned());
         }
         println!("{} New Wallets: {:?}", WALLET_COUNT, start_time.elapsed());
 
@@ -365,5 +499,113 @@ mod tests {
         assert_eq!(wm2.wallets.len(), 0);
         wm2.unlock(default_password.to_owned()).unwrap();
         assert_eq!(wm2.wallets.len(), WALLET_COUNT);
+    }
+
+    #[test]
+    fn test_add_existent_wallet_should_fail() {
+        let default_password = "password";
+
+        let mut wm = WalletManager::new();
+        let wallet = Wallet::new(Chain::KLV).unwrap();
+
+        let result = wm.add_wallet(wallet.clone(), default_password.to_owned());
+        assert!(result.is_ok());
+        let result = wm.add_wallet(wallet.clone(), default_password.to_owned());
+        assert_eq!(
+            result.err(),
+            Some(Error::WalletManagerError(
+                "Wallet already exists".to_string()
+            ))
+        );
+    }
+
+    #[test]
+    fn test_set_mnemonic_should_work() {
+        let default_password = "password".to_string();
+
+        let mut wm = WalletManager::new();
+
+        let result = wm.set_mnemonic(DEFAULT_MNEMONIC.to_string(), default_password);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_set_mnemonic_should_fail() {
+        let default_password = "password".to_string();
+
+        let mut wm = WalletManager::new();
+
+        let result = wm.set_mnemonic("invalid mnemonic".to_string(), default_password);
+        assert_eq!(
+            result.err(),
+            Some(Error::InvalidMnemonic("Invalid mnemonic"))
+        );
+    }
+
+    #[test]
+    fn test_set_mnemonic_already_set_should_fail() {
+        let default_password = "password".to_string();
+
+        let mut wm = WalletManager::new();
+
+        let result = wm.set_mnemonic(DEFAULT_MNEMONIC.to_string(), default_password.clone());
+        assert!(result.is_ok());
+
+        let result = wm.set_mnemonic(DEFAULT_MNEMONIC.to_string(), default_password);
+        assert_eq!(
+            result.err(),
+            Some(Error::WalletManagerError(
+                "Default mnemonic already exists".to_string()
+            ))
+        );
+    }
+
+    #[test]
+    fn test_get_mnemonic_not_set_should_fail() {
+        let default_password = "password".to_string();
+
+        let wm = WalletManager::new();
+        let result = wm.get_mnemonic(default_password);
+        assert_eq!(
+            result.err(),
+            Some(Error::WalletManagerError(
+                "Default mnemonic not found".to_string()
+            ))
+        );
+    }
+
+    #[test]
+    fn test_get_mnemonic_should_work() {
+        let default_password = "password".to_string();
+
+        let mut wm = WalletManager::new();
+
+        let result = wm.set_mnemonic(DEFAULT_MNEMONIC.to_string(), default_password.clone());
+        assert!(result.is_ok());
+
+        let result = wm.get_mnemonic(default_password);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), DEFAULT_MNEMONIC);
+    }
+
+    #[test]
+    fn test_get_mnemonic_wallet() {
+        let default_password = "password".to_string();
+
+        let mut wm = WalletManager::new();
+
+        let result = wm.set_mnemonic(DEFAULT_MNEMONIC.to_string(), default_password.clone());
+        assert!(result.is_ok());
+
+        let mut w_default = wm.wallets.get(KOS_WM_TAG_DEFAULT).unwrap().to_owned();
+        assert!(w_default.is_locked());
+        // get mnemonic shoudl be empty
+        let result = w_default.get_mnemonic();
+        assert_eq!(result, "");
+
+        // unlock wallet and retrive mnemonic
+        let _ = w_default.unlock(default_password);
+        let result = w_default.get_mnemonic();
+        assert_eq!(result, DEFAULT_MNEMONIC);
     }
 }
