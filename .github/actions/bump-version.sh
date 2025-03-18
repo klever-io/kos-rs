96619c513b481d83bc971e09a3b963a197f34d6a
Pedro Camboim <pedrocamboim@klever.io>
2025-03-18 11:59:42 -0300

    feat: bump version to 0.2.12

--- .github/actions/bump-version.sh
+++ .github/actions/bump-version.sh
@@ -9,25 +9,6 @@ MAJOR_VERSION=${ADDR[0]}
 MINOR_VERSION=${ADDR[1]}
 PATCH_VERSION=${ADDR[2]}
 
-# Check the PR title to decide how to bump the version.
-if [[ "${PR_TITLE}" == "MAJOR:"* ]]; then
-  MAJOR_VERSION=$((MAJOR_VERSION + 1))
-  MINOR_VERSION=0
-  PATCH_VERSION=0
-elif [[ "${PR_TITLE}" == "MINOR:"* ]]; then
-  MINOR_VERSION=$((MINOR_VERSION + 1))
-  PATCH_VERSION=0
-else
-  PATCH_VERSION=$((PATCH_VERSION + 1))
-fi
-
-# Construct the new version string.
 NEW_VERSION="$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
 
-# Replace the version in Cargo.toml
-sed -i "s/version = \"$CURRENT_VERSION\"/version = \"$NEW_VERSION\"/g" Cargo.toml
-
-# Print the new version to a file (or alternatively, set it as an environment variable or output variable).
 echo $NEW_VERSION > VERSION
-   
-echo "($PR_TITLE) updating $CURRENT_VERSION to $NEW_VERSION"
