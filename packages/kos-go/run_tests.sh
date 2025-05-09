7ff1ef2c3726faa0c903c205c8538c6dfb1c9293
Patrick Monteiro <patrick@klever.io>
2025-05-09 15:33:32 -0300

    kos-go: ensure test script correct working dir

--- packages/kos-go/run_tests.sh
+++ packages/kos-go/run_tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
-export LIB_DIR=../../target/debug
+set -euo pipefail
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LIB_DIR="$SCRIPT_DIR/../../target/debug"
 
 export CGO_LDFLAGS="-L$LIB_DIR -lkos_mobile"
 
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}
 export LD_LIBRARY_PATH="$LIB_DIR:$LD_LIBRARY_PATH"
 
 go test -v
