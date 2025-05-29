b81cb9d6b72dc2bc56f739c42a6ecfc59bdd2080
Patrick Monteiro <patrick@klever.io>
2025-05-29 10:26:42 -0300

    add android parallel build

--- packages/kos-mobile/build_android.sh
+++ packages/kos-mobile/build_android.sh
@@ -5,6 +5,14 @@ source build_source.sh
 set -o pipefail
 set -e
 
+if command -v nproc >/dev/null 2>&1; then
+  CARGO_JOBS=$(nproc)
+elif command -v sysctl >/dev/null 2>&1; then
+  CARGO_JOBS=$(sysctl -n hw.ncpu)
+else
+  CARGO_JOBS=4
+fi
+
 configure_cargo() {
   cd "$BUILD_HOME"
   for i in $(seq 0 $((${#ANDROID_TOOLCHAINS[@]} - 1))); do
@@ -32,7 +40,7 @@ assemble_android_lib() {
   export OPENSSL_LIB_DIR="$OPENSSL_GENERATED_LIBS_PATH/$toolchain"
   export CC="$ANDROID_NDK_PATH/toolchains/llvm/prebuilt/$OS/bin/$toolchain$ANDROID_MIN_API-clang"
   log_status "assembling android lib to $toolchain..."
-  cargo build --target "$rust_toolchain" --profile mobile -q
+  cargo build --target "$rust_toolchain" --profile mobile -j "$CARGO_JOBS" -q
   export CC=""
   mkdir -p "$ANDROID_JNI_LIBS_PATH"
   mkdir -p "$ANDROID_JNI_LIBS_PATH/$jni"
@@ -44,7 +52,7 @@ assemble_android_lib_unit_test() {
   jni="$JNI_PLATFORM"
   log_status "assembling android test lib..."
   export OPENSSL_LIB_DIR="$OPENSSL_GENERATED_LIBS_PATH/$OS_TOOLCHAIN"
-  cargo build --profile mobile -q
+  cargo build --profile mobile -j "$CARGO_JOBS" -q
   mkdir -p "$ANDROID_JNI_LIBS_PATH/$jni"
   cp -f ../../target/mobile/lib"$PACKAGE_NAME"."$LIB_EXTENSION" "$ANDROID_JNI_LIBS_PATH"/"$jni"
 }
@@ -53,7 +61,7 @@ generate_binds() {
   cd "$BUILD_HOME"
   log_status "generating android binds..."
   export OPENSSL_LIB_DIR="$OPENSSL_GENERATED_LIBS_PATH/$OS_TOOLCHAIN"
-  cargo run -q --bin uniffi-bindgen generate --library ../../target/"${ANDROID_TOOLCHAINS[0]}"/mobile/lib"${PACKAGE_NAME}".so --language kotlin --out-dir android_binds
+  cargo run -j "$CARGO_JOBS" -q --bin uniffi-bindgen generate --library ../../target/"${ANDROID_TOOLCHAINS[0]}"/mobile/lib"${PACKAGE_NAME}".so --language kotlin --out-dir android_binds
   mkdir -p "$ANDROID_GENERATED_BINDS_PATH"
   cp -f -r android_binds/* "$ANDROID_GENERATED_BINDS_PATH"
   rm -rf android_binds
