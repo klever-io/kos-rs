07c11ead7830adf91306bc8310fe5fe986443a8d
Patrick Monteiro <patrick@klever.io>
2025-09-23 14:16:02 -0300

    fix musl testing rule

--- packages/kos-go/kos_mobile/cgo.go
+++ packages/kos-go/kos_mobile/cgo.go
@@ -3,7 +3,7 @@ package kos_mobile
 /*
 #cgo CFLAGS: -I. -I${SRCDIR}
 #cgo linux,!musl LDFLAGS: -L${SRCDIR}/lib/linux-amd64 -lkos_mobile -Wl,-rpath,${SRCDIR}/lib/linux-amd64
-#cgo linux,musl LDFLAGS: -L${SRCDIR}/lib/linux-musl-amd64 -lkos_mobile
+#cgo linux,musl LDFLAGS: -L${SRCDIR}/lib/linux-musl-amd64 -lkos_mobile -Wl,-rpath,${SRCDIR}/lib/linux-musl-amd64
 #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/lib/darwin-amd64 -lkos_mobile -Wl,-rpath,${SRCDIR}/lib/darwin-amd64
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/lib/darwin-aarch64 -lkos_mobile -Wl,-rpath,${SRCDIR}/lib/darwin-aarch64
 #include "kos_mobile.h"
