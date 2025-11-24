8b5d89b27e6ca035023b1726d3f00c1feea48efa
Patrick Monteiro <patrick@klever.io>
2025-11-24 18:08:47 -0300

    simplify parse of signed extensions

--- packages/kos-codec/src/chains/substrate/models.rs
+++ packages/kos-codec/src/chains/substrate/models.rs
@@ -5,13 +5,6 @@ const SIGNED_FLAG: u8 = 0b1000_0000;
 const TRANSACTION_VERSION: u8 = 4;
 const PUBLIC_KEY_TYPE: u8 = 0x00;
 
-#[derive(Debug)]
-enum PaymentExtension {
-    ChargeAssetTxPayment(Option<u32>), // asset_id (None means use 0)
-    ChargeTransactionPayment,          // don't add asset_id
-    None,                              // use default behavior
-}
-
 /// Represents the payload of a Substrate extrinsic (transaction) that will be signed.
 #[allow(dead_code)]
 pub struct ExtrinsicPayload {
@@ -45,20 +38,21 @@ impl ExtrinsicPayload {
         encoded.extend(Compact(self.nonce).encode());
         encoded.extend(Compact(self.tip).encode());
 
-        match self.get_payment_extension() {
-            PaymentExtension::ChargeAssetTxPayment(asset_id_opt) => {
-                // Use asset_id from extension, or 0 if None
-                let asset_id = asset_id_opt.unwrap_or(0);
+        let has_charge_asset_tx_payment =
+            parse_signed_extensions(&self.signed_extensions, "ChargeAssetTxPayment".to_string());
+
+        if let Ok(true) = has_charge_asset_tx_payment {
+            if let Some(asset_id) = self.asset_id {
                 encoded.extend(Compact(asset_id).encode());
+            } else {
+                encoded.extend(Compact(0 as u32).encode());
             }
-            PaymentExtension::ChargeTransactionPayment => {
-                // Don't add asset_id for ChargeTransactionPayment
-            }
-            PaymentExtension::None => {
-                // Default behavior: use self.asset_id if present
-                if let Some(asset_id) = self.asset_id {
-                    encoded.extend(Compact(asset_id).encode());
-                }
+        }
+
+        let has_app_id = parse_signed_extensions(&self.signed_extensions, "AppId".to_string());
+        if let Ok(true) = has_app_id {
+            if let Some(app_id) = self.app_id {
+                encoded.extend(Compact(app_id).encode());
             }
         }
 
@@ -99,26 +93,22 @@ impl ExtrinsicPayload {
         encoded.extend_from_slice(&Compact(self.nonce).encode());
         encoded.extend_from_slice(&Compact(self.tip).encode());
 
-        // Check signed extensions to determine payment method
-        match self.get_payment_extension() {
-            PaymentExtension::ChargeAssetTxPayment(asset_id_opt) => {
-                // Use asset_id from extension, or 0 if None
-                let asset_id = asset_id_opt.unwrap_or(0);
+        let has_charge_asset_tx_payment =
+            parse_signed_extensions(&self.signed_extensions, "ChargeAssetTxPayment".to_string());
+
+        if let Ok(true) = has_charge_asset_tx_payment {
+            if let Some(asset_id) = self.asset_id {
                 encoded.extend(Compact(asset_id).encode());
-            }
-            PaymentExtension::ChargeTransactionPayment => {
-                // Don't add asset_id for ChargeTransactionPayment
-            }
-            PaymentExtension::None => {
-                if let Some(asset_id) = self.asset_id {
-                    encoded.extend(Compact(asset_id).encode());
-                }
+            } else {
+                encoded.extend(Compact(0 as u32).encode());
             }
         }
 
-        // Use the app_id if it is set for AVAIL transactions, otherwise use the mode
-        if let Some(app_id) = self.app_id {
-            encoded.extend_from_slice(Compact(app_id).encode().as_slice());
+        let has_app_id = parse_signed_extensions(&self.signed_extensions, "AppId".to_string());
+        if let Ok(true) = has_app_id {
+            if let Some(app_id) = self.app_id {
+                encoded.extend(Compact(app_id).encode());
+            }
         }
 
         if let Some(mode) = self.mode {
@@ -134,50 +124,24 @@ impl ExtrinsicPayload {
 
         complete_encoded
     }
-
-    /// Determines the payment extension type from signed_extensions
-    fn get_payment_extension(&self) -> PaymentExtension {
-        match parse_signed_extensions(&self.signed_extensions) {
-            Ok(extension) => extension,
-            Err(e) => {
-                eprintln!("Error parsing signed extensions: {}", e);
-                PaymentExtension::None
-            }
-        }
-    }
 }
 
-/// Parses signed extensions and returns the payment extension type
+/// Parses signed extensions and returns the if payment extension type exists
 fn parse_signed_extensions(
     signed_extensions: &Option<Vec<u8>>,
-) -> Result<PaymentExtension, Box<dyn std::error::Error>> {
+    field: String,
+) -> Result<bool, Box<dyn std::error::Error>> {
     let bytes = match signed_extensions {
         Some(b) => b,
-        None => return Ok(PaymentExtension::None),
+        None => return Ok(false),
     };
 
     let json_str = std::str::from_utf8(bytes)?;
     let json: Value = serde_json::from_str(json_str)?;
 
-    if let Some(charge_asset_value) = json.get("ChargeAssetTxPayment") {
-        let asset_id = if charge_asset_value.is_null() {
-            None
-        } else if let Some(obj) = charge_asset_value.as_object() {
-            obj.get("asset_id")
-                .and_then(|v| v.as_u64())
-                .map(|n| n as u32)
-        } else if let Some(num) = charge_asset_value.as_u64() {
-            Some(num as u32)
-        } else {
-            None
-        };
-
-        return Ok(PaymentExtension::ChargeAssetTxPayment(asset_id));
+    if json.get(&field).is_some() {
+        return Ok(true);
     }
 
-    if json.get("ChargeTransactionPayment").is_some() {
-        return Ok(PaymentExtension::ChargeTransactionPayment);
-    }
-
-    Ok(PaymentExtension::None)
+    Ok(false)
 }
