bacf426d153720eb20fb16ef22146bc70c983459
Gustavo Britto <gustavocbritto@gmail.com>
2026-08-26 16:35:02 -0300

    refactor: migrate RNG implementation to support deterministic signing with schnorrkel and add unit tests.

--- packages/kos/src/crypto/rng.rs
+++ packages/kos/src/crypto/rng.rs
@@ -1,20 +1,60 @@
-use rand_core::CryptoRng;
+#[cfg(not(feature = "ksafe"))]
+pub fn getrandom_or_panic() -> impl rand_core_0_6::RngCore + rand_core_0_6::CryptoRng {
+    rand_core_0_6::OsRng
+}
 
-#[cfg(feature = "ksafe")]
-use rand_core::{TryCryptoRng, TryRng};
-
-#[cfg(feature = "ksafe")]
+#[cfg(all(feature = "ksafe", not(test)))]
 extern "C" {
-    #[allow(dead_code)]
     fn random_buffer(p_buffer: *mut u8, size: u32);
 }
 
-#[cfg(feature = "ksafe")]
-#[allow(dead_code)]
-struct MyRng;
+#[cfg(all(feature = "ksafe", test))]
+#[no_mangle]
+unsafe extern "C" fn random_buffer(p_buffer: *mut u8, size: u32) {
+    let slice = core::slice::from_raw_parts_mut(p_buffer, size as usize);
+    for (i, byte) in slice.iter_mut().enumerate() {
+        *byte = (i as u8).wrapping_add(42);
+    }
+}
 
 #[cfg(feature = "ksafe")]
-impl TryRng for MyRng {
+pub struct MyRng;
+
+#[cfg(feature = "ksafe")]
+impl rand_core_0_6::RngCore for MyRng {
+    fn next_u32(&mut self) -> u32 {
+        let mut buf = [0u8; 4];
+        unsafe {
+            random_buffer(buf.as_mut_ptr(), buf.len() as u32);
+        }
+        u32::from_ne_bytes(buf)
+    }
+
+    fn next_u64(&mut self) -> u64 {
+        let mut buf = [0u8; 8];
+        unsafe {
+            random_buffer(buf.as_mut_ptr(), buf.len() as u32);
+        }
+        u64::from_ne_bytes(buf)
+    }
+
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        unsafe {
+            random_buffer(dest.as_mut_ptr(), dest.len() as u32);
+        }
+    }
+
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand_core_0_6::Error> {
+        self.fill_bytes(dest);
+        Ok(())
+    }
+}
+
+#[cfg(feature = "ksafe")]
+impl rand_core_0_6::CryptoRng for MyRng {}
+
+#[cfg(feature = "ksafe")]
+impl rand_core::TryRng for MyRng {
     type Error = core::convert::Infallible;
 
     fn try_next_u32(&mut self) -> Result<u32, Self::Error> {
@@ -42,16 +82,9 @@ impl TryRng for MyRng {
 }
 
 #[cfg(feature = "ksafe")]
-impl TryCryptoRng for MyRng {}
-
-#[cfg(not(feature = "ksafe"))]
-#[allow(dead_code)]
-pub fn getrandom_or_panic() -> impl CryptoRng {
-    rand_core::UnwrapErr(rand::rngs::SysRng)
-}
+impl rand_core::TryCryptoRng for MyRng {}
 
 #[cfg(feature = "ksafe")]
-#[allow(dead_code)]
-pub fn getrandom_or_panic() -> impl CryptoRng {
+pub fn getrandom_or_panic() -> impl rand_core_0_6::RngCore + rand_core_0_6::CryptoRng {
     MyRng
 }
